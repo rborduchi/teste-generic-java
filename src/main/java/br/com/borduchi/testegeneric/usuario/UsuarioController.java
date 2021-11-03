4083d121bb3498ceb33dcc0d8730eb8828360b1c
Rafael Borduchi <rafael.borduchi@myorgtech.com>
2021-11-03 12:21:34 -0300

    Inserido ApiOperator

--- src/main/java/br/com/borduchi/testegeneric/usuario/UsuarioController.java
+++ src/main/java/br/com/borduchi/testegeneric/usuario/UsuarioController.java
@@ -1,5 +1,6 @@
 package br.com.borduchi.testegeneric.usuario;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -17,27 +18,32 @@ public abstract class UsuarioController<T extends GenericUsuario<T>> {
     }
 
     @GetMapping
+    @ApiOperation("Busca paginada dos usuários")
     public Page<T> findPage(Pageable pageable) {
         return usuarioService.findPage(pageable);
     }
 
     @GetMapping("/{id}")
+    @ApiOperation("Busca usuário por id")
     public T findByid(@PathVariable UUID id) {
         return usuarioService.findById(id);
     }
 
     @PostMapping
+    @ApiOperation("Salva um novo usuário")
     public T save(@RequestBody T usuario) {
         return usuarioService.save(usuario);
     }
 
     @PutMapping("/{id}")
+    @ApiOperation("Atualiza um usuário existente")
     public T update(@PathVariable UUID id, @RequestBody T usuario) {
         return usuarioService.update(id, usuario);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiOperation("Delete um usuário pelo id")
     public void delete(@PathVariable UUID id) {
         usuarioService.delete(id);
     }
